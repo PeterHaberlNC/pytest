9f36a668bcfa97b747c2d1113ee4e1c85c139c5f
PeterHaberlNC <118348721+PeterHaberlNC@users.noreply.github.com>
2024-10-11 12:10:37 +0200

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,7 +1,7 @@
 kafka_setting = {
-    'sasl_plain_username': 'alikafka_post_public_intl-sg-i5c3yd4gi01',
-    'sasl_plain_password': 'iDXFIrIp6Apcmn1tF5u6wLmkLSekU6lo',
-    'bootstrap_servers': ["alikafka-post-public-intl-sg-i5c3yd4gi01-1.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-i5c3yd4gi01-2.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-i5c3yd4gi01-3.alikafka.aliyuncs.com:9093"],
+    'sasl_plain_username': 'alikafka_post_public_intl-sg-jiy3ydewp02',
+    'sasl_plain_password': 'sx8z9Czdor9ByyvZXmYeKJK4f40Ad3za',
+    'bootstrap_servers': ["alikafka-post-public-intl-sg-jiy3ydewp02-1.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-jiy3ydewp02-2.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-jiy3ydewp02-3.alikafka.aliyuncs.com:9093"],
     'topic_name': 'kafka-vwpoc-topic',
     'consumer_id': 'Consumer1'
 }
