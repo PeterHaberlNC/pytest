89adff83dcceec7e7dfd1cdd45a8d9c47b87eed7
Peter Haberl <peter.haberl@nordcloud.com>
2024-10-11 12:56:43 +0200

    restructured dir

--- setting.py
+++
@@ -1,7 +0,0 @@
-kafka_setting = {
-    'sasl_plain_username': 'alikafka_post_public_intl-sg-jiy3ydewp02',
-    'sasl_plain_password': 'sx8z9Czdor9ByyvZXmYeKJK4f40Ad3za',
-    'bootstrap_servers': ["alikafka-post-public-intl-sg-jiy3ydewp02-1.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-jiy3ydewp02-2.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-jiy3ydewp02-3.alikafka.aliyuncs.com:9093"],
-    'topic_name': 'kafka-vwpoc-topic',
-    'consumer_id': 'Consumer1'
-}
