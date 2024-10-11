6753891b9f13a0dc6689631ffe9b9103277709e4
Peter Haberl <peter.haberl@nordcloud.com>
2024-10-11 13:25:35 +0200

    testing

---
+++ aliyun_kafka_consumer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import ssl
+from kafka import KafkaConsumer
+import setting
+import six
+
+conf = setting.kafka_setting
+
+context = ssl.create_default_context()
+context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+## The new python(2.7.8+) may cannot ignore the hostname check,
+## you could set to ssl.CERT_NONE to walk around the problem,
+## or you can change the client to confluent-python-demo
+
+#context.verify_mode = ssl.CERT_NONE
+context.verify_mode = ssl.CERT_REQUIRED
+
+context.check_hostname = False
+context.load_verify_locations("mix-4096-ca-cert")
+
+consumer = KafkaConsumer(bootstrap_servers=conf['bootstrap_servers'],
+                         group_id=conf['consumer_id'],
+                         api_version=(0, 10),
+                         session_timeout_ms=25000,
+                         max_poll_records=100,
+                         fetch_max_bytes=1 * 1024 * 1024,
+                         security_protocol='SASL_SSL',
+                         sasl_mechanism="PLAIN",
+                         ssl_context=context,
+                         sasl_plain_username=conf['sasl_plain_username'],
+                         sasl_plain_password=conf['sasl_plain_password'])
+
+print('consumer start to consuming...')
+consumer.subscribe(topics=(conf['topic_name']))
+for message in consumer:
+    print("Topic:[%s] Partition:[%d] Offset:[%d] Value:[%s]" %
+          (message.topic, message.partition, message.offset, message.value))
---
+++ setting.py
@@ -0,0 +1,7 @@
+kafka_setting = {
+    'sasl_plain_username': 'alikafka_post_public_intl-sg-i5c3yd4gi01',
+    'sasl_plain_password': 'iDXFIrIp6Apcmn1tF5u6wLmkLSekU6lo',
+    'bootstrap_servers': ["alikafka-post-public-intl-sg-i5c3yd4gi01-1.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-i5c3yd4gi01-2.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-i5c3yd4gi01-3.alikafka.aliyuncs.com:9093"],
+    'topic_name': 'kafka-vwpoc-topic',
+    'consumer_id': 'Consumer1'
+}
