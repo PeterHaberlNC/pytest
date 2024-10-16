24d4cc9465a6a45d8d2eaa58b673b53b436b1106
PeterHaberlNC <peter.haberl@nordcloud.com>
2024-10-16 13:42:59 +0200

    bootstrap servers array handling

--- setting.py
+++ setting.py
@@ -1,5 +1,7 @@
 import os
-bootstrap_servers = os.getenv('bootstrap_servers', '')
+
+bootstrap_servers_s =  os.getenv('bootstrap_servers', '')
+bootstrap_servers = bootstrap_servers_s.split(',')
 print(f"servers: {bootstrap_servers}")
 topic_name = os.getenv('topic_name', '')
 print(f"topic_name: {topic_name}")
@@ -13,7 +15,7 @@ print(f"sasl_plain_password: {password}")
 kafka_setting = {
     'sasl_plain_username': username,
     'sasl_plain_password': password,
-    'bootstrap_servers': [bootstrap_servers],
+    'bootstrap_servers': bootstrap_servers,
     'topic_name': topic_name,
     'consumer_id': consumer_group
 }
